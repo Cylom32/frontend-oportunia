8d69314c5bc42e5eb57f91de9277e6a2dd4c4cf2
Wilson Muñoz <wilson.munoz.gracia@est.una.ac.cr>
2025-03-27 09:33:02 -0600

    asd
    asdd

--- app/src/main/java/com/example/oportunia/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/oportunia/ui/screens/SettingsScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,6 +30,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,24 +38,66 @@ import androidx.compose.ui.unit.sp
 import com.example.oportunia.R
 import com.example.oportunia.presentation.navigation.NavRoutes
 import com.example.oportunia.ui.theme.OportunIATheme
+import com.example.oportunia.ui.theme.blackPanter
 import com.example.oportunia.ui.theme.lilBlue
+import com.example.oportunia.ui.theme.lilGray
 import com.example.oportunia.ui.theme.walterWhite
+import com.example.oportunia.ui.viewmodel.StudentState
 
 
 //@Preview
 @Composable
 fun SettingScreen(modifier: Modifier = Modifier){
-    Column(
-        modifier = modifier
+
+    Surface(
+        modifier = Modifier
             .fillMaxSize()
-            .background(Color.LightGray.copy(alpha = 0.2f)),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        HeaderSectionSettings()
-        Spacer(modifier = Modifier.weight(1f))
-        ButtonSectionSettings()
-        Spacer(modifier = Modifier.weight(1f))
+            .background(lilGray)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(lilGray),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            // Encabezado
+            Box(
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth()
+                    .shadow(
+                        elevation = 8.dp,
+                        shape = RoundedCornerShape(bottomStart = 32.dp, bottomEnd = 32.dp),
+                        clip = false
+                    )
+                    .background(
+                        color = lilBlue,
+                        shape = RoundedCornerShape(bottomStart = 32.dp, bottomEnd = 32.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.titleSettings),
+                    color = walterWhite,
+                    fontSize = 39.sp,
+                    //  fontWeight = FontWeight.Bold
+                )
+            }
+
+
+
+            Spacer(modifier = Modifier.height(25.dp))
+            // Buttons Section
+            ButtonSectionSettings()
+        }
     }
+
+
+
+
+
+
 
 }
 
@@ -92,7 +136,7 @@ fun HeaderSectionSettings() {
 fun ButtonSectionSettings() {
     Column(
         modifier = Modifier
-            .fillMaxWidth(0.95F)
+            .fillMaxWidth()
             .padding(16.dp), // Eliminamos .height(90.dp) para que la columna crezca según el contenido
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -100,13 +144,13 @@ fun ButtonSectionSettings() {
         Button(
             onClick = { /* Handle Edit Account action */ },
             modifier = Modifier
-                .fillMaxWidth(0.95f)
-                .height(90.dp), // Reducimos la altura para que sea consistente con los otros botones
+                .fillMaxWidth()
+                .height(125.dp), // Reducimos la altura para que sea consistente con los otros botones
             colors = ButtonDefaults.buttonColors(
                 containerColor = walterWhite,
                 contentColor = Color.Black
             ),
-            shape = RoundedCornerShape(12.dp),
+            shape = RoundedCornerShape(34.dp),
             elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp)
         ) {
             Icon(
@@ -115,20 +159,23 @@ fun ButtonSectionSettings() {
                 modifier = Modifier.size(32.dp),
                 tint = Color.Black
             )
-            Spacer(modifier = Modifier.width(12.dp))
+            Spacer(modifier = Modifier.width(20.dp))
             Text(text = "Cuenta", fontSize = 22.sp, color = Color.Black)
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
 
         Button(
             onClick = { /* Handle Edit Language action */ },
             modifier = Modifier
-                .fillMaxWidth(0.95f)
-                .height(90.dp),
+                .fillMaxWidth()
+                .height(125.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = walterWhite,
                 contentColor = Color.Black
             ),
-            shape = RoundedCornerShape(12.dp),
+            shape = RoundedCornerShape(34.dp),
             elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp)
         ) {
             Icon(
@@ -136,21 +183,22 @@ fun ButtonSectionSettings() {
                 contentDescription = "Edit Account",
                 modifier = Modifier.size(32.dp)
             )
-            Spacer(modifier = Modifier.width(12.dp))
+            Spacer(modifier = Modifier.width(34.dp))
             Text(text = "Idioma", fontSize = 22.sp)
         }
 
+        Spacer(modifier = Modifier.height(16.dp))
 
         Button(
             onClick = { /* Handle Logout action */ },
             modifier = Modifier
-                .fillMaxWidth(0.95f)
-                .height(90.dp),
+                .fillMaxWidth()
+                .height(125.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = walterWhite, // Cambiamos el color de fondo a walterWhite
                 contentColor = Color.Black // Cambiamos el color del contenido a negro
             ),
-            shape = RoundedCornerShape(12.dp), // Añadimos la misma forma que el botón "Cuenta"
+            shape = RoundedCornerShape(34.dp), // Añadimos la misma forma que el botón "Cuenta"
             elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp) // Añadimos la misma elevación
         ) {
             Image(
