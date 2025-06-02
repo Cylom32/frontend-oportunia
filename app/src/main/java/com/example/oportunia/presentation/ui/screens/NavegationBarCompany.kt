54bf5e57b1737b212feba29683f561f60a787cfb
Wilson Muñoz <wilson.munoz.gracia@est.una.ac.cr>
2025-06-02 12:48:23 -0600

    Se agregó una barra de navegación para empresa
    asasdasdsad

---
+++ app/src/main/java/com/example/oportunia/presentation/ui/screens/NavegationBarCompany.kt
@@ -0,0 +1,38 @@
+package com.example.oportunia.presentation.ui.screens
+
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ChatBubbleOutline
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.oportunia.presentation.navigation.NavRoutes
+
+@Composable
+fun NavegationBarCompany(
+    selectedScreen: String,
+    onScreenSelected: (String) -> Unit
+) {
+    NavigationBar {
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
+            selected = selectedScreen == NavRoutes.HomeScreenS.ROUTE,
+            onClick = { onScreenSelected(NavRoutes.HomeScreenS.ROUTE) }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.ChatBubbleOutline, contentDescription = "Mensajes") },
+            selected = selectedScreen == NavRoutes.Notifications.ROUTE,
+            onClick = { onScreenSelected(NavRoutes.Notifications.ROUTE) }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Settings, contentDescription = "Settings") },
+            selected = selectedScreen == NavRoutes.Settings.ROUTE,
+            onClick = { onScreenSelected(NavRoutes.Settings.ROUTE) }
+        )
+    }
+}
